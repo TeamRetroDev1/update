17788690234c696cb40f1647a25d428f784cdfdc
Team RetroDev <31925400+TeamRetroDev1@users.noreply.github.com>
2017-09-14 17:47:34 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!bin/bash
 echo mise a jour
-sed -i "s,version='2.1',version='2.2'," /home/retrobuntu/TeamRetroDev/update.sh
+#sed -i "s,version='2.1',version='2.2'," /home/retrobuntu/TeamRetroDev/update.sh
+#sed -i "s,vers='2.0',vers='2.1'," /home/retrobuntu/TeamRetroDev/update.sh
 exit
