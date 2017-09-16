9a56637bc40ef23e2583321f7734dcd0a125ab3d
Team RetroDev <31925400+TeamRetroDev1@users.noreply.github.com>
2017-09-16 14:01:48 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 echo mise a jour
-#sed -i "s,version='2.1',version='2.2'," /home/retrobuntu/TeamRetroDev/update.sh
-#sed -i "s,vers='2.0',vers='2.1'," /home/retrobuntu/TeamRetroDev/update.sh
+sed -i "s,version='2.1',version='2.1.1'," /home/retrobuntu/TeamRetroDev/update.sh
+#sed -i "s,vers='2.1',vers='2.1'," /home/retrobuntu/TeamRetroDev/update.sh
 
 cp *.mp4 $HOME/.attract/layout/Media/Main\ Menu/Video/
 cp *.cfg $HOME/.attract/
