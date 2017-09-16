964e7a4c9600978f04ef855f771b063945a00cde
Team RetroDev <31925400+TeamRetroDev1@users.noreply.github.com>
2017-09-16 16:23:02 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 echo mise a jour
-sed -i "s,version=2.1,version=2.1.1," $HOME/retrobuntu/TeamRetroDev/update.sh
+sed -i "s,version=2.1,version=2.1.1," $HOME/TeamRetroDev/update.sh
 #sed -i "s,vers='2.1',vers='2.1'," /home/retrobuntu/TeamRetroDev/update.sh
 
 cp *.mp4 $HOME/.attract/layouts/Media/Main\ Menu/Video/
