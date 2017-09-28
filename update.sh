9a2873bb5137c53f38a9fa848efbdab685d3c9f9
Team RetroDev <31925400+TeamRetroDev1@users.noreply.github.com>
2017-09-28 18:02:58 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,7 @@
 #!bin/bash
 echo mise a jour
-#sed -i "s,version=2.1,version=2.1.1," $HOME/TeamRetroDev/update.sh
-#sed -i "s,vers='2.1',vers='2.1'," /home/retrobuntu/TeamRetroDev/update.sh
-
-sudo apt-get install build-essential pkg-config git libfontconfig1-dev libopenal-dev libsfml-dev libavutil-dev libavcodec-dev libavformat-dev libavfilter-dev libswscale-dev libavresample-dev libjpeg-dev libglu1-mesa-dev
-git clone http://github.com/mickelson/attract attract
-cd attract
-make -j 3
-sudo make install
+sed -i "s,version=2.1,version=2.1.1," $HOME/TeamRetroDev/update.sh
+sed -i "s,vers='2.1',vers='2.1'," /home/retrobuntu/TeamRetroDev/update.sh
+cp -y x attract.cfg $HOME/.attract/
 sleep 30
 exit
