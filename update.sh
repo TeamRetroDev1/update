13cfcfb2487dd6af311b36c49b6bcbde107e092e
Team RetroDev <31925400+TeamRetroDev1@users.noreply.github.com>
2017-09-28 18:16:38 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!bin/bash
-echo mise a jour
-sed -i "s,version=2.1,version=2.1.1," $HOME/TeamRetroDev/update.sh
-sed -i "s,vers='2.1',vers='2.1'," /home/retrobuntu/TeamRetroDev/update.sh
 cp attract.cfg $HOME/.attract/
+cp version2.1.1.sh $HOME/TeamRetroDev/update.sh
 sleep 30
 exit
